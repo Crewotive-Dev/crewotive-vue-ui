315574cdaccd4b19feb904af0a697667253286ce
akif ayçiçek <akifaycicek@gmail.com>
2021-06-10 00:26:23 +0300

    bootstrap js imported storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 // import '!style-loader!css-loader!sass-loader!@/assets/css/ui/main.scss';
 require('./main.scss')
 import '@/directives/UI/UIDirectives.js'
+import '@/assets/js/bootstrap/bootstrap.esm.min.js'
 
 export const parameters = {
   docs: {
