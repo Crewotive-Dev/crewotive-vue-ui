3b39c653399d81778dd67a43b3bb7ffa8ab08e7e
akif ayçiçek <akifaycicek@gmail.com>
2021-06-10 18:44:28 +0300

    Alert component added with story

---
+++ src/components/UI/CreAlert.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: "CreAlert",
+  props: {
+    variant: {
+      required: false,
+      type: String,
+      default: "primary",
+    },
+    tint: {
+      required: false,
+      type: [String, Number],
+      default: "",
+    },
+    dismissible: {
+      required: false,
+      type: Boolean
+    }
+  },
+  computed:{
+    styleClasses(){
+      const vm = this
+      return{
+        [`alert-dismissible`]: vm.dismissible,
+        [`alert-${vm.variant}${vm.tint && `-${vm.tint}`}`]: vm.variant
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <div
+    class="alert"
+    :class="styleClasses"
+    role="alert"
+  >
+    <slot />
+    <button
+      data-bs-dismiss="alert"
+      class="btn btn-close"
+      v-if="dismissible"
+    />
+  </div>
+</template>
--- src/plugins/globalComponents.js
+++ src/plugins/globalComponents.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 
-Vue.component('CreRow', ()=> import('@/components/UI/CreRow.vue'));
-Vue.component('CreCol', ()=> import('@/components/UI/CreCol.vue'));
-Vue.component('CreText', ()=> import('@/components/UI/CreText.vue'));
-Vue.component('CreButton', ()=> import('@/components/UI/CreButton.vue'));
-Vue.component('CreContainer', ()=> import('@/components/UI/CreContainer.vue'));
 Vue.component('CreAccordion', ()=> import('@/components/UI/accordion/CreAccordion.vue'));
+Vue.component('CreAlert', ()=> import('@/components/UI/CreAlert.vue'));
 Vue.component('CreAccordionItem', ()=> import('@/components/UI/accordion/CreAccordionItem.vue'));
+Vue.component('CreButton', ()=> import('@/components/UI/CreButton.vue'));
+Vue.component('CreCol', ()=> import('@/components/UI/CreCol.vue'));
+Vue.component('CreContainer', ()=> import('@/components/UI/CreContainer.vue'));
+Vue.component('CreRow', ()=> import('@/components/UI/CreRow.vue'));
+Vue.component('CreText', ()=> import('@/components/UI/CreText.vue'));
