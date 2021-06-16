52073ef2687082b3ab978a8e86a9fcc3bad9b04e
akif ayçiçek <akifaycicek@gmail.com>
2021-06-17 00:44:20 +0300

    Carousel component added with story

---
+++ src/components/UI/carousel/CreCarousel.vue
@@ -0,0 +1,62 @@
+<script>
+export default {
+  name: "CreCarousel",
+  data() {
+    return {
+      id: "carousel_"+Math.random().toString(16).substring(2,10)
+    };
+  },
+  props: {
+    buttons:{
+      required: false,
+      type: Boolean,
+      default: true
+    }
+  },
+  computed:{
+    styleClasses(){
+      const vm = this
+      return [
+        {
+        }
+      ]
+    }
+  }
+};
+</script>
+
+<template>
+  <div
+    :id="id"
+    class="carousel slide overflow-hidden"
+    :class="styleClasses"
+  >
+    <div class="carousel-inner">
+      <slot />
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      :data-bs-target="`#${id}`"
+      v-if="buttons"
+      data-bs-slide="prev"
+    >
+      <span
+        class="carousel-control-prev-icon"
+        aria-hidden="true"
+      />
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      :data-bs-target="`#${id}`"
+      v-if="buttons"
+      data-bs-slide="next"
+    >
+      <span
+        class="carousel-control-next-icon"
+        aria-hidden="true"
+      />
+    </button>
+  </div>
+</template>
---
+++ src/components/UI/carousel/CreCarouselItem.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  name: "CreCarouselItem",
+  props: {
+    active:{
+      required: false,
+      type: Boolean,
+      default: false
+    },
+    imgSrc:{
+      required: false,
+      type: String,
+      default: ""
+    },
+    imgAlt:{
+      required: false,
+      type: String,
+      default: "carousel image"
+    },
+  },
+  computed:{
+    styleClasses(){
+      const vm = this
+      return [
+        {
+          'active': vm.active
+        }
+      ]
+    }
+  }
+};
+</script>
+
+<template>
+  <div
+    class="carousel-item"
+    :class="styleClasses"
+  >
+    <img
+      :src="imgSrc"
+      class="d-block w-100"
+      :alt="imgAlt"
+    >
+    <div class="carousel-caption d-none d-md-block">
+      <slot name="carousel-caption" />
+      <slot />
+    </div>
+  </div>
+</template>
