7571fcc61b2f0ccec243754cf527e72c7103c255
akif ayçiçek <akifaycicek@gmail.com>
2021-07-01 17:02:29 +0300

    `List Group` and `List Group Item` components were added with their story

---
+++ src/components/UI/list-group/CreListGroup.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+  name: 'CreListGroup',
+  props:{
+    flush: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    numbered:{
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    horizontal:{
+      type: [Boolean, String],
+      required: false,
+      default: false
+    }
+  },
+  computed:{
+    styleClasses(){
+      const vm = this
+      return {
+        [`list-group-flush`]: vm.flush,
+        [`list-group-numbered`]: vm.numbered,
+        [`list-group-horizontal${vm.horizontal == 'xs' ? '':'-'+vm.horizontal }`]: vm.horizontal,
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <ul
+    class="list-group"
+    :class="styleClasses"
+  >
+    <slot />
+  </ul>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/UI/list-group/CreListGroupItem.vue
@@ -0,0 +1,56 @@
+<script>
+  export default {
+    name: "CreListGroupItem",
+    props:{
+      variant:{
+        type: String,
+        required: false,
+        default: ''
+      },
+      tint:{
+        type: [String, Number],
+        required: false,
+        default: '5'
+      },
+      action:{
+        type: Boolean,
+        required: false,
+        default: false
+      },
+      active:{
+        type: Boolean,
+        required: false,
+        default: false
+      },
+      disabled:{
+        type: Boolean,
+        required: false,
+        default: false
+      }
+    },
+    computed:{
+      styleClasses(){
+        const vm = this
+        return{
+          [`list-group-item-${vm.variant}${vm.tint && '-'+vm.tint}`]: vm.variant,
+          [`list-group-item-action`]: vm.action,
+          [`active`]: vm.active,
+          [`disabled`]: vm.disabled,
+        }
+      }
+    }
+  }
+</script>
+<template>
+  <li
+    class="list-group-item"
+    :role="action && 'button'"
+    :class="styleClasses"
+  >
+    <slot />
+  </li>
+</template>
+
+<style>
+
+</style>
